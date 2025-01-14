4dd7f158dcad462530ad5b1ab8a5171c50d0cda0
Folajimi Anifowose <fanifowo@iu.edu>
2025-01-13 22:05:56 -0500

    Practice for lab 0

---
+++ Lab_0_Anifowose_folajimi.R
@@ -0,0 +1,20 @@
+8+3
+log(2)
+((121/3)*(6^3))/(pi)
+#create x and y
+x= 8+3
+y=log(2)
+
+#calculation
+x+y
+#define and return z
+z= x*y
+x= 8+3; x
+X=21;x
+
+a=c(4.1,6.7,8.2,1.8); a
+b= 2*a; b
+
+mean(a)
+sd(a)
+plot(a,b)
